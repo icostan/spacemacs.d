75108898a6db67de87562d4cae12175a27b9e732
Iulian Costan <iulian.costan@gmail.com>
2018-05-08 20:44:50 +0300

    Disable spacemacs banner, set ruby-indent to 2

--- init.el
+++ init.el
@@ -127,7 +127,7 @@ values."
    ;; directory. A string value must be a path to an image format supported
    ;; by your Emacs build.
    ;; If the value is nil then no banner is displayed. (default 'official)
-   dotspacemacs-startup-banner 'official
+   dotspacemacs-startup-banner nil
    ;; List of items to show in startup buffer or an association list of
    ;; the form `(list-type . list-size)`. If nil then it is disabled.
    ;; Possible values for list-type are:
@@ -337,7 +337,7 @@ you should place your code here."
   (global-company-mode)
 
   (setq-default
-   git-magit-status-fullscreen t
+   ruby-indent-level 2
    powerline-default-separator 'bar
    ;; js2-mode
    js2-basic-offset 2
