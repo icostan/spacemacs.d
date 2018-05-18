8737e74fc741bc4f0401857a2798b511bd75c2aa
Iulian Costan <iulian.costan@gmail.com>
2018-05-18 19:39:48 +0300

    Add dash layer

--- init.el
+++ init.el
@@ -41,6 +41,7 @@ values."
      react
      themes-megapack
      cucumber
+     dash
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
